66b610d228a546a142fd897949e34492252616d8
Mateus Nunes <mateus.n@rksoftware.com.br>
2026-03-30 15:31:50 -0300

    Refactor nodes.go to improve readability

--- ast/nodes.go
+++ ast/nodes.go
@@ -12,29 +12,29 @@ type Binary struct {
 	Right    Expression
 }
 
-type Grouping struct {
-	Expression Expression
-}
-
-type Literal struct {
-	Value interface{}
-}
-
-type Unary struct {
-	Operator token.Token
-	Right    Expression
-}
-
 func (b *Binary) Accept(visitor Visitor) interface{} {
 	return visitor.VisitBinary(b)
+}
+
+type Grouping struct {
+	Expression Expression
 }
 
 func (g *Grouping) Accept(visitor Visitor) interface{} {
 	return visitor.VisitGrouping(g)
 }
 
+type Literal struct {
+	Value interface{}
+}
+
 func (l *Literal) Accept(visitor Visitor) interface{} {
 	return visitor.VisitLiteral(l)
+}
+
+type Unary struct {
+	Operator token.Token
+	Right    Expression
 }
 
 func (u *Unary) Accept(visitor Visitor) interface{} {
