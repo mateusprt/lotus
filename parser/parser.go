2570da6e2666c7776dd7fa7780d5459a7935abcc
mateusprt <matnun95@gmail.com>
2026-04-11 11:17:49 -0300

    Add for loop

--- parser/parser.go
+++ parser/parser.go
@@ -64,6 +64,9 @@ func varDeclaration(p *Parser) *ast.VarStmt {
 }
 
 func statement(p *Parser) ast.Stmt {
+	if match(p, token.FOR) {
+		return forStatement(p)
+	}
 	if match(p, token.IF) {
 		return ifStatement(p)
 	}
@@ -77,6 +80,70 @@ func statement(p *Parser) ast.Stmt {
 		return &ast.BlockStmt{Statements: block(p)}
 	}
 	return expressionStatement(p)
+}
+
+func forStatement(p *Parser) ast.Stmt {
+	consume(p, token.LPAREN, "Expect '(' after 'for'.")
+
+	var initializer ast.Stmt
+	if match(p, token.SEMICOLON) {
+		initializer = nil
+	} else if match(p, token.VAR) {
+		initializer = varDeclaration(p)
+	} else {
+		initializer = expressionStatement(p)
+	}
+
+	var condition ast.Expression
+	if !check(p, token.SEMICOLON) {
+		expr, err := expression(p)
+		if err != nil {
+			panic(err)
+		}
+		condition = expr
+	}
+	consume(p, token.SEMICOLON, "Expect ';' after loop condition.")
+
+	var increment ast.Expression
+	if !check(p, token.RPAREN) {
+		expr, err := expression(p)
+		if err != nil {
+			panic(err)
+		}
+		increment = expr
+	}
+	consume(p, token.RPAREN, "Expect ')' after for clauses.")
+
+	body := statement(p)
+
+	// Desugar o loop for em um loop while
+	if increment != nil {
+		body = &ast.BlockStmt{
+			Statements: []ast.Stmt{
+				body,
+				&ast.ExpressionStmt{Expression: increment},
+			},
+		}
+	}
+
+	if condition == nil {
+		condition = &ast.Literal{Value: true}
+	}
+
+	body = &ast.WhileStmt{
+		Condition: condition,
+		Body:      body,
+	}
+
+	if initializer != nil {
+		body = &ast.BlockStmt{
+			Statements: []ast.Stmt{
+				initializer,
+				body,
+			},
+		}
+	}
+	return body
 }
 
 func block(p *Parser) []ast.Stmt {
